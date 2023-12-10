81233ba19e46091362e0fa21b0712e7f56af3193
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-12-10 21:38:06 +0300

    added welcome section

---
+++ src/constants/perks.ts
@@ -0,0 +1,22 @@
+import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
+
+export const perks = [
+  {
+    Icon: ArrowDownToLine,
+    name: "Instant Delivery",
+    description:
+      "Get your assets delivered to your email in seconds and download them right away.",
+  },
+  {
+    Icon: CheckCircle,
+    name: "Guaranteed Quality",
+    description:
+      "Every asset on our platform is verified by our team to ensure our highest quality standards. Not happy? We offer a 30-day refund guarantee.",
+  },
+  {
+    Icon: Leaf,
+    name: "For the Planet",
+    description:
+      "We've pledged 1% of sales to the preservation and reservation of the natural environment.",
+  },
+];
